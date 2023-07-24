31baa1e3aeba6ecd512498618ef63c13ad74fbf4
Medet Ahmetson <admin@blocklords.io>
2023-07-24 16:18:16 +0300

    Add sample extension for calculating the parameters

---
+++ handler/handler.go
@@ -0,0 +1,62 @@
+package handler
+
+import (
+	"github.com/ahmetson/common-lib/data_type/key_value"
+	"github.com/ahmetson/service-lib/communication/command"
+	"github.com/ahmetson/service-lib/communication/message"
+	"github.com/ahmetson/service-lib/log"
+	"github.com/ahmetson/service-lib/remote"
+)
+
+var onAdd = func(request message.Request, _ *log.Logger, _ ...*remote.ClientSocket) message.Reply {
+	x, err := request.Parameters.GetUint64("x")
+	if err != nil {
+		return message.Fail("request.Parameters: " + err.Error())
+	}
+
+	y, err := request.Parameters.GetUint64("y")
+	if err != nil {
+		return message.Fail("request.Parameters: " + err.Error())
+	}
+
+	sum := x + y
+
+	parameters := key_value.Empty()
+	parameters.Set("z", sum)
+
+	return message.Reply{
+		Status:     message.OK,
+		Message:    "",
+		Parameters: parameters,
+	}
+}
+
+var onMul = func(request message.Request, _ *log.Logger, _ ...*remote.ClientSocket) message.Reply {
+	x, err := request.Parameters.GetUint64("x")
+	if err != nil {
+		return message.Fail("request.Parameters: " + err.Error())
+	}
+
+	y, err := request.Parameters.GetUint64("y")
+	if err != nil {
+		return message.Fail("request.Parameters: " + err.Error())
+	}
+
+	sum := x * y
+
+	parameters := key_value.Empty()
+	parameters.Set("z", sum)
+
+	return message.Reply{
+		Status:     message.OK,
+		Message:    "",
+		Parameters: parameters,
+	}
+}
+
+func Add() *command.Route {
+	return command.NewRoute("add", onAdd)
+}
+func Mul() *command.Route {
+	return command.NewRoute("mul", onMul)
+}
--- main.go
+++ main.go
@@ -1,1 +1,30 @@
 package main
+
+import (
+	"github.com/ahmetson/sample-extension/handler"
+	"github.com/ahmetson/service-lib/configuration"
+	"github.com/ahmetson/service-lib/extension"
+	"github.com/ahmetson/service-lib/log"
+)
+
+func main() {
+	logger, _ := log.New("sample", false)
+	appConfig, _ := configuration.New(logger)
+
+	service, _ := extension.New(appConfig, logger.Child("extension"))
+	service.AddController(configuration.ReplierType)
+
+	service.GetController().AddRoute(handler.Add())
+	service.GetController().AddRoute(handler.Mul())
+
+	webProxy := "github.com/ahmetson/web-proxy"
+	service.RequireProxy(webProxy)
+	service.Pipe(webProxy, service.GetControllerName())
+
+	err := service.Prepare()
+	if err != nil {
+		logger.Fatal("service.Prepare", "error", err)
+	}
+
+	service.Run()
+}
